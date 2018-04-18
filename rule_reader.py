72f9f2be8d8c4e2f0e2fb5bfbae5c111a833115f
aceawan <ladeveze.quentin@openmailbox.org>
2018-04-18 15:14:54 +0200

    add the possibility to print all the rules

--- rule_reader.py
+++ rule_reader.py
@@ -27,12 +27,12 @@ def int2base(x, base):
     return ''.join(digits)
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
-		print("Usage : python rule_reader.py <dat_file> <rule number>")
+	if (len(sys.argv) < 2) or (len(sys.argv) > 3):
+		print("Usage :\n\tpython rule_reader.py <dat_file> <rule number> for a specif\n\tpython rule_reader.py <dat_file> for all the rules")
 		sys.exit(1)
 
+
 	dat_path = str(sys.argv[1])
-	rule_number = int(sys.argv[2]) - 1
 
 	f = open(dat_path, "rb")
 	raw_data = f.read()
@@ -42,17 +42,27 @@ if __name__ == "__main__":
 	n_state = reversible_rules[0]
 	n_vois = reversible_rules[1]
 	n_rules = reversible_rules[2]
+	duration = reversible_rules[3]
 
 	print("{} states and {} neighbours".format(n_state, n_vois))
 	print("{} reversible rules".format(n_rules))
+	print("enumeration lasted {} seconds".format(duration))
 
-	if rule_number >= n_rules:
-		print('we only have {} rules'.format(n_rules))
-		exit(2)
 
 	nb_config = n_state ** n_vois
-
 	configs = [format(int2base(x, n_state)).rjust(n_vois, '0') for x in range(0, nb_config)]
 
-	print(reversible_rules[3][rule_number])
-	print(list(zip(configs, reversible_rules[3][rule_number])))
+	if(len(sys.argv) == 3):
+		rule_number = int(sys.argv[2]) - 1
+		
+		if rule_number >= n_rules:
+			print('we only have {} rules'.format(n_rules))
+			exit(2)
+
+		print(reversible_rules[4][rule_number])
+		print(list(zip(configs, reversible_rules[4][rule_number])))
+
+	if(len(sys.argv) == 2):
+		for i in range(0, n_rules):
+			print(reversible_rules[4][i])
+			print(list(zip(configs, reversible_rules[4][i])))
